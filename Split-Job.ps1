4fb286b83a8014db77d6d1c6e6394331bcae1883
Arnoud Jansveld <ajansveld@gmail.com>
2008-06-19 18:01:14 -0400

    many updates

--- Split-Job.ps1
+++ Split-Job.ps1
@@ -1,25 +1,40 @@
 #requires -version 1.0
-###################################################################################################
+################################################################################
 ## Run commands in multiple concurrent pipelines
-##   by Arnoud Jansveld
+##   by Arnoud Jansveld - www.jansveld.net/powershell
 ## Version History
-## 0.9    Includes logic to distinguish between scriptblocks and cmdlets or scripts. If a ScriptBlock
-##        is specified, a foreach {} wrapper is added
+## 0.92   Add UseProfile switch: imports the PS profile into each runspace
+##        Add Variable parameter: imports variable(s) into each runspace
+##        Add Alias parameter: imports alias(es)
+##        Restart pipeline if it stops due to an error
+##        Set the current path in each runspace to that of the calling process
+## 0.91   Revert to v 0.8 input syntax for the script block
+##        Add error handling for empty input queue
+## 0.9    Add logic to distinguish between scriptblocks and cmdlets or scripts:
+##        if a ScriptBlock is specified, a foreach {} wrapper is added
 ## 0.8    Adds a progress bar
 ## 0.7    Stop adding runspaces if the queue is already empty
 ## 0.6    First version. Inspired by Gaurhoth's New-TaskPool script
-###################################################################################################
+################################################################################
 
 function Split-Job (
     $Scriptblock = $(throw 'You must specify a command or script block!'),
-    [int]$MaxPipelines=10
+    [int]$MaxPipelines=10,
+    [switch]$UseProfile,
+    [string[]]$Variable,
+    [string[]]$Alias
+
 ) {
     # Create the shared thread-safe queue and fill it with the input objects
-    $Queue = [System.Collections.Queue]::Synchronized([System.Collections.Queue]@($Input))
+    $Queue = [Collections.Queue]::Synchronized([Collections.Queue]@($Input))
     $QueueLength = $Queue.Count
-    # Set up the script to be run by each pipeline
-    if ($Scriptblock -is [ScriptBlock]) {$Scriptblock = "foreach {$Scriptblock}"}
-    $Script = '$Queue = $($Input); & {while ($Queue.Count) {$Queue.Dequeue()}} | ' + $Scriptblock
+    if ($MaxPipelines -gt $QueueLength) {$MaxPipelines = $QueueLength}
+    # Set up the script to be run by each runspace
+    $Script  = "Set-Location '$PWD'; "
+    $Script += '$Queue = $($Input); '
+    $Script += '& {trap {continue}; while ($Queue.Count) {$Queue.Dequeue()}} |'
+    $Script += $Scriptblock
+
     # Create an array to keep track of the set of pipelines
     $Pipelines = New-Object System.Collections.ArrayList
 
@@ -28,10 +43,29 @@ function Split-Job (
         # It will automatically start processing objects from the shared queue.
         $Runspace = [System.Management.Automation.Runspaces.RunspaceFactory]::CreateRunspace($Host)
         $Runspace.Open()
-        $PipeLine = $Runspace.CreatePipeline($Script)
+        # Optionally import profile, variables and aliases from the main runspace
+        if ($UseProfile) {
+            $Pipeline = $Runspace.CreatePipeline(". '$PROFILE'")
+            $Pipeline.Invoke()
+            $Pipeline.Dispose()
+        }
+        if ($Variable) {
+            Get-Variable $Variable -Scope 2 | foreach {
+                trap {continue}
+                $Runspace.SessionStateProxy.SetVariable($_.Name, $_.Value)
+            }
+        }
+        if ($Alias) {
+            $Pipeline = $Runspace.CreatePipeline({$Input | Set-Alias -value {$_.Definition}})
+            $Null = $Pipeline.Input.Write((Get-Alias $Alias -Scope 2), $True)
+            $Pipeline.Input.Close()
+            $Pipeline.Invoke()
+            $Pipeline.Dispose()
+        }
+        $Pipeline = $Runspace.CreatePipeline($Script)
         $Null = $Pipeline.Input.Write($Queue)
         $Pipeline.Input.Close()
-        $PipeLine.InvokeAsync()
+        $Pipeline.InvokeAsync()
         $Null = $Pipelines.Add($Pipeline)
     }
 
@@ -43,7 +77,7 @@ function Split-Job (
     }
 
     # Start the pipelines
-    do {Add-Pipeline} until ($Pipelines.Count -ge $MaxPipelines -or $Queue.Count -eq 0)
+    while ($Pipelines.Count -lt $MaxPipelines -and $Queue.Count) {Add-Pipeline}
 
     # Loop through the pipelines and pass their output to the pipeline until they are finished
     while ($Pipelines.Count) {
@@ -52,10 +86,12 @@ function Split-Job (
         foreach ($Pipeline in (New-Object System.Collections.ArrayList(,$Pipelines))) {
             if ( -not $Pipeline.Output.EndOfPipeline -or -not $Pipeline.Error.EndOfPipeline ) {
                 $Pipeline.Output.NonBlockingRead()
-                $Pipeline.Error.NonBlockingRead() | foreach {Write-Error $_}
+                $Pipeline.Error.NonBlockingRead() | Write-Error
             } else {
                 if ($Pipeline.PipelineStateInfo.State -eq 'Failed') {
                     Write-Error $Pipeline.PipelineStateInfo.Reason
+                    # Start a new runspace, unless there was a syntax error in the scriptblock
+                    if ($Queue.Count -lt $QueueLength) {Add-Pipeline}
                 }
                 Remove-Pipeline $Pipeline
             }
